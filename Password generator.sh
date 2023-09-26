2368bedf1622f26fc8556aa6cc2eca7983b2dee0
Black shadow <117249911+Blackshadownbachi@users.noreply.github.com>
2023-09-26 22:52:37 +0530

    Create Password generator.sh

---
+++ Password generator.sh
@@ -0,0 +1,12 @@
+GNU nano 7.2                           shadow.sh
+cowsay telugu hacks group|lolcat
+echo        "password genrator tool"
+sleep 1
+echo "enter password lenght :"
+read PASS
+for p in $(seq 1 99);
+do
+    openssl rand -base64 48 | cut -c1-$PASS
+done
+
+echo "completed" | lolcat
